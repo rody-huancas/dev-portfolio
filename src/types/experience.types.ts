a1926e47b1eb53f7dd8e3836ad820ceda59f97d0
Rody Huancas <rodyhuancas.04@gmail.com>
2026-01-21 23:29:25 -0500

    feat: add IExperience interface to define experience structure

---
+++ src/types/experience.types.ts
@@ -0,0 +1,10 @@
+export interface IExperience {
+  id            : number;
+  company       : string;
+  position      : string;
+  period        : string;
+  description   : string;
+  technologies  : string[];
+  websiteUrl    : string;
+  certificateUrl: string;
+}
