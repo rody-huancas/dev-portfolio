3760c3c3a65c5c9addab6c601ae7a7788a0d070d
Rody Huancas <rodyhuancas.04@gmail.com>
2026-01-22 17:19:24 -0500

    update: adjust padding and margin in main layout for better responsiveness

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -6,7 +6,7 @@ const AppLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     <>
       <Header />
 
-      <main className='max-w-4xl mx-auto my-24 px-5 sm:px-0'>
+      <main className='max-w-4xl mx-auto md:my-24 px-7 xl:px-0'>
         {children}
       </main>
 
