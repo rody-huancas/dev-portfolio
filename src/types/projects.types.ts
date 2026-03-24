2530b19d61e8f4743fd8137cee47a73fd57c420c
Rody Huancas <rodyhuancas.04@gmail.com>
2026-03-24 18:45:50 -0500

    refactor(project-types): change project description type to LocalizedText

--- src/types/projects.types.ts
+++ src/types/projects.types.ts
@@ -1,7 +1,9 @@
+import type { LocalizedText } from "@/types/i18n.types";
+
 export interface IProject {
   id          : string;
   title       : string;
-  description : string;
+  description : LocalizedText;
   technologies: string[];
   githubUrl   : string;
   liveUrl     : string;
