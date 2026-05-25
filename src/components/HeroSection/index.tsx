80a1cf08b17fc270520c0d1692f320937cacd898
Rody Huancas <rodyhuancas.04@gmail.com>
2026-05-24 22:19:45 -0500

    fix(hero.section): update resume link to use new Cloudinary asset URL

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -122,7 +122,7 @@ const HeroSection = () => {
             </a>
 
             <a
-              href="https://res.cloudinary.com/dmgazox1n/image/upload/v1769735355/Portfolio/Documents/CV%20-%20HILDER%20RODY%20HUANCAS%20CHUQUIPOMA.pdf"
+              href="https://asset.cloudinary.com/dmgazox1n/6054b3dbb2e40bab099c610b542b3383"
               aria-label={dictionary.hero.resume.ariaLabel}
               title={dictionary.hero.resume.title}
               target="_blank"
